37b97d87f6f1211f3cb7c56dbbaa28adb00439b8
conrad casper <cnrdcasper@gmail.com>
2018-12-17 15:26:21 -0500

    trying to look up some stuff

--- user.js
+++ user.js
@@ -2,7 +2,7 @@ let letterMemory = []
 
 
 
-// from joe kim
+// from joseph kim
 $('.letter').on('click', function () {
     let letterValue = $(this).html()
     letterMemory.push(letterValue)
@@ -11,7 +11,12 @@ $('.letter').on('click', function () {
     var str = "PERSISTENCE"
   var n = str.includes(letterValue);
   console.log(n)
+  if (n === true){
+      
+  }
 })
 // I need to create a variable to see if the letters that I'm pushing into the array are matching up with the letters in the word that that variable represents
-
+// If the character selected is in the mystery word it should appear on the screen, and the key should be disabled
+// If the character selected is in the mystery word it should appear on the screen
+// How? 
 
