6be470e6e14612252986e9e2e4d3e6244dcbec74
conrad casper <cnrdcasper@gmail.com>
2018-12-17 11:47:13 -0500

    everything is fucked up

--- gamelogic.js
+++ gamelogic.js
@@ -1,1 +1,8 @@
 
+document.getElementById("1").style.zIndex = "7";
+document.getElementById("2").style.zIndex = "6";
+document.getElementById("3").style.zIndex = "5";
+document.getElementById("4").style.zIndex = "4";
+document.getElementById("5").style.zIndex = "3";
+document.getElementById("6").style.zIndex = "2";
+document.getElementById("7").style.zIndex = "1";
