a533cb1aa103fde8da37eab9c1480986997a8b74
conrad casper <cnrdcasper@gmail.com>
2018-12-17 15:03:38 -0500

    added some stuff

--- user.js
+++ user.js
@@ -1,10 +1,17 @@
+let letterMemory = []
+
 
 
 // from joe kim
 $('.letter').on('click', function () {
-    let letterValue = $(this).html();
-    console.log(letterValue);})
+    let letterValue = $(this).html()
+    letterMemory.push(letterValue)
+    console.log(letterValue)
+    console.log(letterMemory)
+    var str = "PERSISTENCE"
+  var n = str.includes(letterValue);
+  console.log(n)
+})
+// I need to create a variable to see if the letters that I'm pushing into the array are matching up with the letters in the word that that variable represents
 
-    const newGame = function(){
-    
-    }
+
