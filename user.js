be81be7e484ee6301a4bca905e368d0e169c6334
conrad casper <cnrdcasper@gmail.com>
2018-12-17 20:14:09 -0500

    changes so far

--- user.js
+++ user.js
@@ -12,6 +12,7 @@ $('.letter').on('click', function () {
   if (n >= 0){
      letterMemory[n] = letterValue
      $('#letterDisplay').html(letterMemory)
+
   } if (n < 0) {
      wrongCount += 1;
   }
@@ -19,7 +20,7 @@ $('.letter').on('click', function () {
   console.log(wrongCount)
 })
 
-console.log(letterMemory)
+
 // I need to create a variable to see if the letters that I'm pushing into the array are matching up with the letters in the word that that variable represents
 // If the character selected is in the mystery word it should appear on the screen, and the key should be disabled
 // If the character selected is in the mystery word it should appear on the screen
